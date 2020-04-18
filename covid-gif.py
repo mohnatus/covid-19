eee252b2a81deb8a8a9e8c6e914c63883bb947f1
FurryCat <furrycat.web@gmail.com>
2020-04-18 12:28:20 +0300

    add comments

--- covid-gif.py
+++ covid-gif.py
@@ -1,34 +1,34 @@
-# Импорт библиотек
+# Секция 1. Импорт библиотек
 import pandas as pd
 import matplotlib.pyplot as plt
 import glob
 import moviepy.editor as mpy
 
-# Преобразование данных в датафреймы
+# Секция 2. Преобразование данных в датафреймы
 df = pd.read_csv('us-states.csv', parse_dates=['date'])
 populations = pd.read_csv('nst-est2019-alldata.csv', usecols=['NAME', 'POPESTIMATE2019'])
 
-# Мерж данных по населению; пересчет на 100 тыс.
+# Секция 3. Мерж данных по населению; пересчет на 100 тыс.
 df = pd.merge(df, populations, how = 'left', left_on = 'state', right_on = 'NAME')
 df['rate'] = df['cases'] / df['POPESTIMATE2019'] * 100000
 
-# Отбор штатов
-df_today = df[df['date'] == '2020-04-16']
-topfivestates_rate = list(df_today.sort_values(by='rate', ascending=False).head()['state'])
+# Секция 4. Отбор штатов
+df_1604 = df[df['date'] == '2020-04-16']
+topfivestates_rate = list(df_1604.sort_values(by='rate', ascending=False).head()['state'])
 topfivestates_rate.append('California')
 topfivestates_rate.append('Washington')
 
-# Фильтрация датасета
+# Секция 5. Фильтрация датасета
 df = df[df['state'].isin(topfivestates_rate)]
 df = df[df['date'] >= '2020-03-01']
 df = df.pivot(index = 'date', columns = 'state', values = 'rate')
 
-# Подготовка данных к отображению
+# Секция 6. Подготовка данных к отображению
 df = df.reset_index()
 df = df.reset_index(drop=True)
 df = df.drop(columns = 'date')
 
-# Построение графиков
+# Секция 7. Построение графиков
 plt.style.use('fivethirtyeight')
 length = len(df.index)
 for i in range(10,length+10):
@@ -42,7 +42,7 @@ for i in range(10,length+10):
     fig = ax.get_figure()
     fig.savefig(f"pngs/{i}.png")
 
-# Генерация GIF
+# Секция 8. Генерация GIF
 gif_name = 'COVID'
 fps = 6
 file_list = glob.glob('pngs/*')
